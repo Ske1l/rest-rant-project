e19fc7477b62854f7e700aaf35b41c53dcd02ac6
Ske1l <hayley6236@gmail.com>
2022-07-18 22:16:07 -0400

    adding bootstrap grid to index

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 REST-Rant is an app where users can review restaurants.
 
 //Your places should have a name (string), city (string), state (string), cuisines (string) and a pic (string)
+
+source for H-Thai pic: Photo by Jerome Jome on Unsplash.com
+source for coding cat cafe pic: Photo by bobotaks on Unsplash.com
--- views/home.jsx
+++ views/home.jsx
@@ -6,13 +6,21 @@ function home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/brenda-godinez-MsTOg6rhRVk-unsplash.jpg" alt="Chia Fruit Shake"
+                        height="700"
+                        width="800" />
+                    <div>
+                        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
             </main>
 
-
         </Def>
     )
 }
+
 module.exports = home
