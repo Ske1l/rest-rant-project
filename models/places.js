1a0c62de410d4fe7982d79ee664ed63348dbf3b0
Ske1l <hayley6236@gmail.com>
2022-07-25 21:19:02 -0400

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,16 @@
+
+
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/jerome-jome-Ga3ziG9jCno-unsplash.jpg'
+
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/bobotaks-kTATJGIbtOE-unsplash.jpg'
+}]
