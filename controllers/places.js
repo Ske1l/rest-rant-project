748c422ee7f66a89e15d7e0edf59732e06b08a3e
Ske1l <hayley6236@gmail.com>
2022-08-06 02:14:16 -0400

    Show Route Connected

--- controllers/places.js
+++ controllers/places.js
@@ -22,6 +22,21 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })
 
+//Show /places
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/show', { place: places[id] })
+
+    }
+})
+
 
 
 
